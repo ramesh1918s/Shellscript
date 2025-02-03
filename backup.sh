64371a83270781746495d488873a7a990376df9c
ramesh1918s <shivram915454@gmail.com>
2025-02-03 17:25:04 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -35,7 +35,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14 )
 
 echo "Files: $FILES"
 
-if [ -z $FILES ] #true if FILES is empty
+if [ ! -z $FILES ] #true if FILES is empty
 then
    echo "Files are found"
 
