ac12549542f6bbb7ea0cb8bbcc17ed99be87d694
ramesh1918s <shivram915454@gmail.com>
2025-02-03 18:49:17 +0530

    backup

--- disk-usage.sh
+++ disk-usage.sh
@@ -41,5 +41,11 @@ DISK_THRESHILD=5 #REAL projects, it is usually  75
 while IFS= read -r line #IFS , internal feild seperatpor ,empty it will not ignore while space . -r is for not ignore special charaters like (/,@.%.#)
 do 
    echo $line
-   #USAGE=$(echo $file)
+   USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+   PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+   if [ $USAGE -ge $DISK_THRESHILD ]
+   then 
+     echo "$PARTITION is more then $DISK_THRESHOLD, current value: $USAGE.Please check"
+
+    fi
 done <<< $DISK_USAGE
